03b1ad9346275f68ddeb16d82dde88dcbb28bf50
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-10-25 19:04:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+//-------------------------------------------------------//
+//(1) Set remainder equal to the remainder of 11 divided // // by 3 using the remainder (%) operator.                //
+//-------------------------------------------------------//
+
+var remainder = 11 % 3;
+
+//-------------------------------------------------------//
+//(2) Convert the assignments for a, b, and c to use the // //+= operator.                                           //
+//-------------------------------------------------------//
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+//-----------------------------------------------------//
+//(3) Convert the assignments for a, b,and c to use the//
+//-= operator                                          //
+//-----------------------------------------------------//
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+
